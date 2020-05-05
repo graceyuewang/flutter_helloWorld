5da8089e6bc0bef5238077229f33a3d760235a8a
graceyueyue <graceyueyue@outlook.com>
2020-05-05 14:20:17 +0800

    over

--- state_less_ful/lib/image_show.dart
+++ state_less_ful/lib/image_show.dart
@@ -2,11 +2,12 @@ import 'package:flutter/material.dart';
 
 class ImageShow extends StatelessWidget {
   final String imgName;
-  const ImageShow({Key key, this.imgName}) : super(key: key);
+  const ImageShow({Key key, @required this.imgName}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Image.asset('assets/$imgName');
+    print(this.imgName);
+    return Image.asset('images/$imgName');
   }
   
 }
--- state_less_ful/lib/main.dart
+++ state_less_ful/lib/main.dart
@@ -8,6 +8,7 @@ void main() => runApp(MyApp());
  }
 
  class _MyAppState extends State<MyApp> {
+   String imageFileName = 'p1.jpg';
    @override
    Widget build(BuildContext context) {
      return MaterialApp(
@@ -19,7 +20,25 @@ void main() => runApp(MyApp());
         body: Center(
           child: Column(
             children:<Widget>[
-
+              //图片显示lessw
+              ImageShow(
+                imgName: imageFileName,
+                ),
+              //按钮
+              RaisedButton(
+                onPressed: () {
+                if (imageFileName == 'p1.jpg') {
+                  imageFileName = 'p2.jpg';
+                } else {
+                  imageFileName = 'p1.jpg';
+                }
+                setState(() {
+                  imageFileName = imageFileName;
+                  print(imageFileName);
+                });
+              },
+                child: Text('切换图片'),
+              ),
             ],
           ),
         ),
